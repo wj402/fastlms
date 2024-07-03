4d23dbc61f0f1cee4bfc86c2a24b43f5f979f765
DESKTOP-M0321IK\HOME <wj402@naver.com>
2024-07-04 01:30:16 +0900

    [Feat] 데이터전송 DB저장
    form태그에서 보내진 회원가입 내용을 JPA를 통해 DB저장
    
    #Resolves: #1

---
+++ src/main/java/com/zerobase/fastlms/member/controller/MemberController.java
@@ -0,0 +1,39 @@
+package com.zerobase.fastlms.member.controller;
+
+import com.zerobase.fastlms.member.entity.Member;
+import com.zerobase.fastlms.member.model.MemberInput;
+import com.zerobase.fastlms.member.repository.MemberRepository;
+import com.zerobase.fastlms.member.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/member/register")
+    public String register() {
+
+        return "member/register";
+    }
+
+    @PostMapping("/member/register")
+    public String registerSubmit(Model model, HttpServletRequest request, MemberInput parameter) {
+
+        boolean result = memberService.register(parameter);
+
+        model.addAttribute("result", result);
+
+        return "member/register_complete";
+    }
+}
