eb9b6ff872233eef803ad76deb8cb062471927b7
DESKTOP-M0321IK\HOME <wj402@naver.com>
2024-07-04 02:04:51 +0900

    [Feat] 메일 전송 설정 및 메일 전송 테스트 구현
    JavaMailSender를 통한 메일 전송 설정 및 메일 전송 테스트 (Gmail용)
    
    #Resolves: #1

--- src/main/java/com/zerobase/fastlms/MainController.java
+++ src/main/java/com/zerobase/fastlms/MainController.java
@@ -11,21 +11,31 @@ package com.zerobase.fastlms;
 
 // http://localhost:8080/
 
+import com.zerobase.fastlms.components.MailComponents;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@RequiredArgsConstructor
 @Controller
 public class MainController {
+
+    private final MailComponents mailComponents;
+
 
     @RequestMapping("/")
     public String index() {
 
+        String email = "wj402@naver.com";
+        String subject = " 안녕하세요. 박찬성입니다.";
+        String text = "<p>안녕하세요</p> <p>반갑습니다.</p>";
 
+        mailComponents.sendMail(email, subject, text);
 
         return "index";
     }
